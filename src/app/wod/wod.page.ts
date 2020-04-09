8946138ed513f7a3178d0fc06a25c6799f509e01
manucoffin <manuel.coffin@gmail.com>
2020-04-09 12:36:01 +0200

    Setup of main pages and navigation

---
+++ src/app/wod/wod.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wod',
+  templateUrl: './wod.page.html',
+  styleUrls: ['./wod.page.scss'],
+})
+export class WodPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
